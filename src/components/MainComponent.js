d62426d31944bf0aa8783c15d68c235e364af6b3
Maqsood Ahemed Khan <maqsoodkhan7299@gmail.com>
2022-09-27 10:29:08 +0530

    Intro to Redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,27 +2,24 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { LEADERS } from '../shared/leaders';
-import { PROMOTIONS } from '../shared/promotion';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
+const mapStateToProps = state => {
+  return { dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+}
+}
 class Main extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            dishes: DISHES,
-            selectedDish: null,
-            comments:COMMENTS,
-            promotions: PROMOTIONS,
-            leaders: LEADERS
-        };
     }
     onDishSelect(dishId){
         this.setState({selectedDish: dishId});
@@ -32,40 +29,33 @@ class Main extends Component {
     render(){
         const HomePage = ()=>{
             return(<Home 
-                dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-                promotion={this.state.promotions.filter((promotion) =>promotion.featured)[0]}
-                leader={this.state.leaders.filter((leader) =>leader.featured)[0]}
+                dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+                promotion={this.props.promotions.filter((promotion) =>promotion.featured)[0]}
+                leader={this.props.leaders.filter((leader) =>leader.featured)[0]}
             />);
         }
         const DishWithId = ({match}) =>{
             return(
-                <DishDetail dish={this.state.dishes.filter((dish)=> dish.id === parseInt(match.params.dishId,10))[0]}
-                    comments={this.state.comments.filter((comment)=> comment.dishId === parseInt(match.params.dishId,10))}
+                <DishDetail dish={this.props.dishes.filter((dish)=> dish.id === parseInt(match.params.dishId,10))[0]}
+                    comments={this.props.comments.filter((comment)=> comment.dishId === parseInt(match.params.dishId,10))}
                 />
             );
         }
    
         return (
             <div>
-                {/* <Navbar dark color="primary">
-                    <div className="container">
-                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-                    </div>
-                </Navbar> */}
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
-                    <Route exact path='/aboutus' component={()=><About leaders={this.state.leaders}/>} />
-                    <Route exact path='/menu' component={()=><Menu dishes={this.state.dishes} />} />
+                    <Route exact path='/aboutus' component={()=><About leaders={this.props.leaders}/>} />
+                    <Route exact path='/menu' component={()=><Menu dishes={this.props.dishes} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
-                {/* { <Menu dishes = { this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> } */}
                 <Footer />
             </div>
         );
     }
 }
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
